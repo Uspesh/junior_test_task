a79e01dd3453035a81ddb10a99e07b881ab82c9b
Neekee <Uspeshniy.ae@yandex.ru>
2023-07-03 20:43:13 +0300

    fix everything for properly work

--- src/src/settings.py
+++ src/src/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from src.config import *
+
+from .config import DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
