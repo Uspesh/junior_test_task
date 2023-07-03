d74b1135ac8aba9dc58f3990644ae62c95f77606
Neekee <Uspeshniy.ae@yandex.ru>
2023-07-03 22:14:54 +0300

    add: employee model

--- src/online_catalog/models.py
+++ src/online_catalog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    full_name = models.CharField(max_length=255, verbose_name='ФИО')
+    work_status = models.CharField(max_length=100, verbose_name='Должность')
+    start_work_time = models.TimeField(auto_now=True, verbose_name='День приема на работу')
+    salary = models.IntegerField(verbose_name='Зар. Плата')
+    boss = models.CharField(verbose_name='Начальник', max_length=50)
